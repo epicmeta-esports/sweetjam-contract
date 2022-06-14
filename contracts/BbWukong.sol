401bb2cfbf40d3fb49734b66e28ed3c68a1ce461
Douglas <douglasswm@gmail.com>
2022-06-14 18:58:05 +0800

    feat: wk contract

--- contracts/BbWukong.sol
+++ contracts/BbWukong.sol
@@ -1,57 +1,139 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract BbWukong is ERC721, Ownable {
+contract BbWukong is ERC721, Pausable, Ownable {
+    using SafeMath for uint256;
 
-    uint256 public mintPrice;
-    uint256 public totalSupply;
-    uint256 public maxSupply;
-    uint256 public maxPerWallet;
-    bool public isPublicMintEnabled;
-    string internal baseTokenUri; 
-    address payable public withdrawWallet;
-    mapping(address => uint256) public walletMint;
+    uint16[88] public ids;
+    uint16 private index;
+    uint256 public PRICE = 0.02 ether;
+    uint256 public WL_PRICE = 0.01 ether;
+    uint256 public constant MAX_PER_MINT = 4;
+    string public baseTokenURI;
+    bool public isWhitelistActive = false;
 
-    constructor() payable ERC721("BbWukong", "BW") {
-        mintPrice = 0.01 ether;
-        totalSupply = 0;
-        maxSupply = 88;
-        maxPerWallet = 2;
-        // set withdraw wallet address
+    mapping(address => uint256) private _whitelist;
+
+    constructor(string memory baseURI) ERC721("BbWukong", "BBW") {
+        setBaseURI(baseURI);
     }
 
-    function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
-        isPublicMintEnabled = isPublicMintEnabled_;
+    // random id logic
+    function _pickRandomUniqueId(uint256 random) private returns (uint256 id) {
+        uint256 len = ids.length - index++;
+        require(len > 0, "No NFTs left");
+        uint256 randomIndex = random % len;
+        id = ids[randomIndex] != 0 ? ids[randomIndex] : randomIndex;
+        ids[randomIndex] = uint16(ids[len - 1] == 0 ? len - 1 : ids[len - 1]);
+        ids[len - 1] = 0;
     }
 
-    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
-        baseTokenUri = baseTokenUri_;
+    // mint functions
+    function _mintSingle() private {
+        uint256 _random = uint256(
+            keccak256(
+                abi.encodePacked(
+                    index++,
+                    msg.sender,
+                    block.timestamp,
+                    blockhash(block.number - 1)
+                )
+            )
+        );
+        _safeMint(msg.sender, _pickRandomUniqueId(_random) + 1);
     }
 
-    function tokenURI(uint256 tokenId_) public view override returns (string memory) {
-        require(_exists(tokenId_), "Token does not exist");
-        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
-    }
+    function mintManyNFT(uint256 _count) public payable {
+        uint256 _purchaseAvailability = MAX_PER_MINT - balanceOf(msg.sender);
+        require(
+            _count > 0 && _count <= _purchaseAvailability,
+            "Inventory exceeded limit."
+        );
+        require(ids.length > _count, "Not enough NFTs left!");
+        require(
+            msg.value >= PRICE.mul(_count),
+            "Not enough ether to purchase NFTs."
+        );
 
-    function withdraw() external onlyOwner {
-        (bool success, ) = withdrawWallet.call{ value: address(this).balance }('');
-        require(success, "withdraw failed");
-    }
-
-    function mint(uint256 quantity_) public payable {
-        require(isPublicMintEnabled, "minting not enabled");
-        require(msg.value == quantity_ * mintPrice, "Wrong mint value");
-        require(totalSupply + quantity_ <= maxSupply, "Sold out");
-        require(walletMint[msg.sender] + quantity_ <= maxPerWallet, "Exceed max per wallet");
-
-        for (uint256 i = 0; i < quantity_; i++) {
-            uint256 newTokenId = totalSupply + 1;
-            _safeMint(msg.sender, newTokenId); 
-            totalSupply++;
+        for (uint256 i = 0; i < _count; i++) {
+            _mintSingle();
         }
+    }
+
+    function mintManyWlNFT(uint256 _count) public payable {
+        require(isWhitelistActive, "Whitelist is not active!");
+        require(
+            _count <= _whitelist[msg.sender],
+            "Exceeded max available to purchase"
+        );
+        require(ids.length > _count, "Not enough NFTs left!");
+        require(
+            msg.value >= WL_PRICE.mul(_count),
+            "Not enough ether to purchase NFTs."
+        );
+
+        for (uint256 i = 0; i < _count; i++) {
+            _mintSingle();
+        }
+        _whitelist[msg.sender] -= _count;
+    }
+
+    // withdraw ether balance from contract
+    function withdraw() public payable onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No ether left to withdraw");
+
+        (bool success, ) = (msg.sender).call{value: balance}("");
+        require(success, "Transfer failed.");
+    }
+
+    // setters
+    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        PRICE = _price;
+    }
+
+    function setWLPrice(uint256 _price) public onlyOwner {
+        WL_PRICE = _price;
+    }
+
+    function setIsWhitelistActive(bool _isWhitelistActive) external onlyOwner {
+        isWhitelistActive = _isWhitelistActive;
+    }
+
+    function setWhitelist(address[] calldata addresses) external onlyOwner {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _whitelist[addresses[i]] = MAX_PER_MINT;
+        }
+    }
+
+    // pausable
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // overrides
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
